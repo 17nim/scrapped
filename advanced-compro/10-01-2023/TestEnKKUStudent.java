b56be7c26e1f6cab2cc6296098e758d8ff705713
17nim <17nim.m@gmail.com>
2023-01-10 20:46:14 +0700

    Add KKUStudent and its subclasses.

---
+++ advanced-compro/10-01-2023/TestEnKKUStudent.java
@@ -0,0 +1,7 @@
+public class TestEnKKUStudent {
+    public static void main(String[] args) {
+        EnKKUStudent siraspon = new EnKKUStudent("Siraspon");
+        siraspon.setGearNumber(59);
+        System.out.print(siraspon);
+    }
+}
