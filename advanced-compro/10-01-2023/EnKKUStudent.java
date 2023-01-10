c6df9af3e68eabb539ca9c17ecca75eda13c585f
17nim <17nim.m@gmail.com>
2023-01-10 20:46:04 +0700

    Add KKUStudent and its subclasses.

---
+++ advanced-compro/10-01-2023/EnKKUStudent.java
@@ -0,0 +1,23 @@
+class EnKKUStudent extends KKUStudent {
+    protected int gearNumber;
+
+    EnKKUStudent() {
+        super();
+    }
+
+    EnKKUStudent(String name) {
+        super(name);
+    }
+
+    public int getGearNumber() {
+        return gearNumber;
+    }
+
+    public void setGearNumber(int newGearNumber) {
+        gearNumber = newGearNumber;
+    }
+
+    public String toString() {
+        return name + " is a KKU Engineering student with gear number as " + gearNumber + ".";
+    }
+}
